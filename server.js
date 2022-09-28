40d8addd431a2b278f4c16d578641ff8b64895ef
Ogi Bin Edi <ogibinedi@gmail.com>
2022-09-28 16:45:23 +0700

    setup routes and controller for user

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 // app.use(express.static('public'));
 
 app.use('/', require('./routes/root'));
+app.use('/users', require('./routes/userRoutes'));
 
 app.all('*', (req, res) => {
     res.status(404);
