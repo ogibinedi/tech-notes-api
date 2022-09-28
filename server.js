9746c5354386f474d5c2d1df46886e9eb16204f6
Ogi Bin Edi <ogibinedi@gmail.com>
2022-09-28 18:47:22 +0700

    Add Notes Controller

--- server.js
+++ server.js
@@ -30,13 +30,14 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/root'));
 app.use('/users', require('./routes/userRoutes'));
+app.use('/notes', require('./routes/noteRoutes'));
 
 app.all('*', (req, res) => {
     res.status(404);
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'));
     } else if (req.accepts('json')) {
-        res.json({ message: '404 Not Found'});
+        res.json({ message: '404 Not Found' });
     } else {
         res.type('txt').send('404 Not Found');
     }
