f349ed2409bdfa4c1cf62028d1e8d6ae96d52653
= <=>
2022-10-03 00:07:21 +0700

    Remove typo string

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,7 +31,7 @@ const login = asyncHandler(async (req, res) => {
             }
         },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: '15m' }
+        { expiresIn: '2m' }
     )
 
     const refreshToken = jwt.sign(
@@ -80,7 +80,7 @@ const refresh = (req, res) => {
                     }
                 },
                 process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '15m' }
+                { expiresIn: '2m' }
             )
 
             res.json({ accessToken })
